9b45e9809d77f31a5e4f35b337b568a29eb9fe11
Tuan Tran <tuantran7700@gmail.com>
2024-05-09 12:54:28 -0700

    finalPush

--- sprite.js
+++ sprite.js
@@ -2,26 +2,24 @@ class Sprite {
     constructor(sprite_json, x, y, start_state) {
         this.sprite_json = sprite_json;
         this.position = new Vector(x, y);
-        this.velocity = new Vector(10, 10); // Initial velocity
+        this.velocity = new Vector(10, 10);
         this.state = start_state;
         this.root_e = "TenderBud";
         this.cur_frame = 0;
-        this.cur_bk_data = null;
-        this.border_hit = false;
         this.collisionCooldown = 0;
     }
 
 
     separate(allSprites) {
-        let desiredSeparation = 50;
-        let steer = new Vector(0, 0);
+        let neighbordist = 50; //width of sprite
+        let steer = new Vector(0, 0); 
         let count = 0;
-        for (let other of allSprites) {
-            let dx = this.position.x - other.position.x;
+        for (let other of allSprites) { //to check all sprites
+            let dx = this.position.x - other.position.x; 
             let dy = this.position.y - other.position.y;
             let d = Math.sqrt(dx * dx + dy * dy);
-            if ((d > 0) && (d < desiredSeparation)) {
-                steer.add(new Vector(dx, dy));
+            if ((d > 0) && (d < neighbordist)) {
+                steer.add(new Vector(dx, dy)); 
                 count++;
             }
         }
@@ -32,65 +30,65 @@ class Sprite {
     }
 
     align(allSprites) {
-        let neighbordist = 50;
-        let averageVel = new Vector(0, 0);
+        let neighbordist = 50; //width of sprites
+        let velo = new Vector(0, 0);
         let count = 0;
-        for (let other of allSprites) {
+        for (let other of allSprites) { //to check all sprites
             let dx = this.position.x - other.position.x;
             let dy = this.position.y - other.position.y;
             let d = Math.sqrt(dx * dx + dy * dy);
             if ((d > 0) && (d < neighbordist)) {
-                averageVel.add(other.velocity);
+                velo.add(other.velocity);
                 count++;
             }
         }
         if (count > 0) {
-            averageVel.div(count);
-            this.velocity.add(averageVel);
+            velo.div(count);
+            this.velocity.add(velo);
         }
     }
 
 
     cohesion(allSprites) {
-        let neighbordist = 50;
-        let centerMass = new Vector(0, 0);
+        let neighbordist = 50; //width of sprites
+        let center = new Vector(0, 0); 
         let count = 0;
-        for (let other of allSprites) {
+        for (let other of allSprites) { //to check all sprites
             let dx = this.position.x - other.position.x;
             let dy = this.position.y - other.position.y;
             let d = Math.sqrt(dx * dx + dy * dy);
             if ((d > 0) && (d < neighbordist)) {
-                centerMass.add(other.position);
+                center.add(other.position);
                 count++;
             }
         }
         if (count > 0) {
-            centerMass.div(count);
-            let moveTowards = Vector.sub(centerMass, this.position);
+            center.div(count);
+            let moveTowards = Vector.sub(center, this.position);
             this.velocity.add(moveTowards);
         }
     }
 
 
-    applyRandomVelocity() {
+    randomVelocity() {
         const speed = 10;
         const centerX = canvas.width / 2;
         const centerY = canvas.height / 2;
         let preferredAngle = Math.random() * Math.PI * 2;
 
-        // NEEDED TO NED GET STUCK ON CLOSEST BOUNDARY
-        if (this.position.x < centerX && this.position.y < centerY) { // Top-left quadrant
+        //to not get stuck
+        if (this.position.x < centerX && this.position.y < centerY) { 
             preferredAngle = (Math.random() * Math.PI / 2) + 0; // 0 to 90 degrees
-        } else if (this.position.x >= centerX && this.position.y < centerY) { // Top-right quadrant
+        } else if (this.position.x >= centerX && this.position.y < centerY) { 
             preferredAngle = (Math.random() * Math.PI / 2) + Math.PI / 2; // 90 to 180 degrees
-        } else if (this.position.x >= centerX && this.position.y >= centerY) { // Bottom-right quadrant
+        } else if (this.position.x >= centerX && this.position.y >= centerY) { 
             preferredAngle = (Math.random() * Math.PI / 2) + Math.PI; // 180 to 270 degrees
-        } else if (this.position.x < centerX && this.position.y >= centerY) { // Bottom-left quadrant
+        } else if (this.position.x < centerX && this.position.y >= centerY) { 
             preferredAngle = (Math.random() * Math.PI / 2) + 3 * Math.PI / 2; // 270 to 360 degrees
         }
 
-        // Defining sectors based on 30 degrees increments and their corresponding walking states
-        const sectors = [
+//dictates direction of the sprites walk
+        const slices = [
             { angle: Math.PI / 6, state: 'walk_E' }, // 0-30
             { angle: Math.PI / 3, state: 'walk_SE' }, //30-60
             { angle: Math.PI / 2, state: 'walk_S' }, //60-90
@@ -105,36 +103,35 @@ class Sprite {
             { angle: 2 * Math.PI, state: 'walk_E' } //330-360
         ];
 
-        let sector = sectors.find(sec => preferredAngle <= sec.angle);
-        sector = sector || sectors[sectors.length - 1];
-        this.state = sector.state;
+        let slice = slices.find(sec => preferredAngle <= sec.angle);
+        slice = slice || slices[slices.length - 1];
+        this.state = slice.state;
         this.velocity = new Vector(Math.cos(preferredAngle) * speed, Math.sin(preferredAngle) * speed);
     }
 
-    // Method to detect collision with another sprite
-    checkCollision(otherSprite) {
+    //detects collision with another sprite
+    collision(otherSprite) {
         const dx = this.position.x - otherSprite.position.x;
         const dy = this.position.y - otherSprite.position.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-        const collisionDistance = 70; // You can adjust this threshold based on sprite size
+        const collisionDistance = 70;
 
         if (this.collisionCooldown > 0 || otherSprite.collisionCooldown > 0) {
             return; // Skip if either sprite is in cooldown
         }
         if (distance < collisionDistance) {
-            console.log("COLLIDED")
-            this.applyRandomVelocity();
-            otherSprite.applyRandomVelocity();
+            this.randomVelocity();
+            otherSprite.randomVelocity();
 
             this.collisionCooldown = 30; // 10 frame cooldown
             otherSprite.collisionCooldown = 30;
         }
     }
 
-    checkCollisions(allSprites) {
+    collisions(allSprites) {
         for (const sprite of allSprites) {
             if (sprite !== this) {
-                this.checkCollision(sprite);
+                this.collision(sprite);
             }
         }
     }
@@ -169,9 +166,10 @@ class Sprite {
         this.separate(allSprites);
         this.align(allSprites);
         this.cohesion(allSprites);
+
         this.position.add(this.velocity);
         this.checkBounds();
-        this.checkCollisions(allSprites);
+        this.collisions(allSprites);
     }
 
     checkBounds() {
@@ -192,7 +190,7 @@ class Sprite {
         }
 
         if (bounced) {
-            this.applyRandomVelocity(); // Apply new random velocity on bounce
+            this.randomVelocity(); // Apply new random velocity on bounce
         }
     }
 
